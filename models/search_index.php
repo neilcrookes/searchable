53bef475602e79a18359e300d5cf93ee5436e6dc
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-01-19 01:20:01 -0500

    updated SearchIndex::getTypes() to use a custom find as a backend for reading types from db

--- models/search_index.php
+++ models/search_index.php
@@ -2,30 +2,43 @@
 class SearchIndex extends SearchableAppModel {
     var $name = 'SearchIndex';
     var $useTable = 'search_index';
+    var $_findMethods = array(
+        'types' => true
+    );
 
 /**
  * Returns array of types (models) used in the Search Index with model name as
  * the key and the humanised form as the value.
  *
- * @return unknown
+ * @return array
  */
     function getTypes() {
-        // Read from cache
-        $types = Cache::read('search_index_types');
-        if ($types !== false) {
+        if (($types = Cache::read('search_index_types')) !== false) {
             return $types;
         }
-        // If cache not valid generate types data
-        $data = $this->find('all', array('fields' => array('DISTINCT(SearchIndex.model)', 'DISTINCT(SearchIndex.model)')));
-        $data = Set::extract('/SearchIndex/model', $data);
-        $types = array();
-        foreach ($data as $type) {
-            $types[$type] = Inflector::humanize(Inflector::tableize($type));
-        }
-        // Store types in cache
-        Cache::write('search_index_types', $types);
-        return $types;
+         return $this->find('types');
+     }
 
+    function _findTypes($state, $query, $results = array()) {
+        if ($state == 'before') {
+            $query['fields'] = array(
+                'DISTINCT(SearchIndex.model)',
+                'DISTINCT(SearchIndex.model)'
+            );
+            return $query;
+        } else if ($state == 'after') {
+            $types = array();
+            $results = Set::extract('/SearchIndex/model', $results);
+
+            foreach ($results as $type) {
+                $types[$type] = Inflector::humanize(Inflector::tableize($type));
+            }
+
+            if (!empty($types)) {
+                Cache::write('search_index_types', $types);
+            }
+            return $types;
+        }
     }
 
 }
