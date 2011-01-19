9df8234d1bed43a408a865410c9cfa5a468d6c7d
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-01-19 01:19:31 -0500

    modified controller action for simpler readability

--- controllers/search_indexes_controller.php
+++ controllers/search_indexes_controller.php
@@ -4,49 +4,57 @@ class SearchIndexesController extends SearchableAppController {
     var $paginate = array('SearchIndex' => array('limit' => 10));
     var $helpers = array('Searchable');
 
-    function index() {
-
+    function index($term = null) {
         // Redirect with search data in the URL in pretty format
         if (!empty($this->data)) {
-            $redirect = array();
-            if (isset($this->data['SearchIndex']['term'])
-            && !empty($this->data['SearchIndex']['term'])) {
-                $redirect['term'] = urlencode(urlencode($this->data['SearchIndex']['term']));
-            } else {
-                $redirect['term'] = 'null';
-            }
-            if (isset($this->data['SearchIndex']['type'])
-            && !empty($this->data['SearchIndex']['type'])) {
+            $redirect = array(
+                'plugin' => $this->params['plugin'],
+                'controller' => $this->params['controller'],
+                'action' => $this->params['index'],
+                'type' => 'All',
+            );
+            if (isset($this->data['SearchIndex']['type']) && !empty($this->data['SearchIndex']['type'])) {
                 $redirect['type'] = $this->data['SearchIndex']['type'];
+            } elseif (isset($this->params['type']) && $this->params['type'] != 'All') {
+                $redirect['type'] = $this->params['type'];
             } else {
                 $redirect['type'] = 'All';
+            }
+
+            if (isset($this->data['SearchIndex']['term']) && !empty($this->data['SearchIndex']['term'])) {
+                $redirect['term'] = $this->data['SearchIndex']['term'];
+            } elseif (isset($this->params['term']) && !empty($this->params['term'])) {
+                $redirect['term'] = $this->params['term'];
+            } else {
+                $redirect['term'] = null;
             }
             $this->redirect($redirect);
         }
 
         // Add default scope condition
-        $this->paginate['SearchIndex']['conditions'] = array('SearchIndex.active' => 1);
-
         // Add published condition NULL or < NOW()
-        $this->paginate['SearchIndex']['conditions']['OR'] = array(
-            array('SearchIndex.published' => null),
-            array('SearchIndex.published <= ' => date('Y-m-d H:i:s'))
-        );
+        $this->paginate = array('SearchIndex' => array(
+                array('SearchIndex.active' => 1),
+                'or' => array(
+                    array('SearchIndex.published' => null),
+                    array('SearchIndex.published <= ' => date('Y-m-d H:i:s'))
+                )
+        ));
 
         // Add type condition if not All
-        if (isset($this->params['type'])
-        && $this->params['type'] != 'All') {
-            $this->data['SearchIndex']['type'] = $this->params['type'];
-            $this->paginate['SearchIndex']['conditions']['model'] = $this->params['type'];
+        if (isset($this->params['type']) && $this->params['type'] != 'All') {
+            $this->data['SearchIndex']['type'] = Sanitize::escape($this->params['type']);
+            $this->paginate['SearchIndex']['conditions']['model'] = $this->data['SearchIndex']['type'];
         }
 
         // Add term condition, and sorting
-        if (isset($this->params['term'])
-        && $this->params['term'] != 'null') {
-            $this->data['SearchIndex']['term'] = $this->params['term'];
+        if (!$term && isset($this->params['term'])) {
             $term = $this->params['term'];
+        }
+        if ($term) {
             App::import('Core', 'Sanitize');
             $term = Sanitize::escape($term);
+            $this->data['SearchIndex']['term'] = $term;
             $this->paginate['SearchIndex']['conditions'][] = "MATCH(data) AGAINST('$term' IN BOOLEAN MODE)";
             $this->paginate['SearchIndex']['fields'] = "*, MATCH(data) AGAINST('$term' IN BOOLEAN MODE) AS score";
             $this->paginate['SearchIndex']['order'] = "score DESC";
@@ -56,9 +64,8 @@ class SearchIndexesController extends SearchableAppController {
 
         // Get types for select drop down
         $types = $this->SearchIndex->getTypes();
-        $this->set(compact('results', 'types'));
+        $this->set(compact('results', 'term', 'types'));
         $this->pageTitle = 'Search';
-
     }
 
 }
