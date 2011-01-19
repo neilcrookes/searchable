1db981818f9ce29fe46ecac38868fca5a92c3b43
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-01-19 01:53:21 -0500

    SearchableHelper::snippet() can now access an $options array that will pass variables to TextHelper methods

--- views/helpers/searchable.php
+++ views/helpers/searchable.php
@@ -2,13 +2,32 @@
 class SearchableHelper extends AppHelper {
     var $helpers = array('Html', 'Text');
 
-    function snippets($data) {
-        $data = json_decode($data, true);
-        $term = trim($this->data['SearchIndex']['term']);
-        $snippets = '';
-        while (strlen($snippets) < 255 && $value = next($data)) {
-            $snippets .= $this->Text->highlight($this->Text->excerpt($value, $term, 20), $term);
+    function snippets($data, $options = array()) {
+        $options = array_merge(array(
+            'field' => 'term',
+            'maxLength' => 255,
+            'radius' => 20,
+            'ending' => '',
+            'highlight' => array(),
+        ), $options);
+
+        if (isset($this->data['SearchIndex'][$options['field']])) {
+            $term = trim($this->data['SearchIndex'][$options['field']]);
+        } else {
+            $term = null;
         }
-        return $snippets;
+
+        $data = json_decode($data, true);
+        $snippets = '';
+
+        while (strlen($snippets) < $options['maxLength'] && $value = next($data)) {
+            $snippets .= ' ' . $this->Text->highlight(
+                $this->Text->excerpt($value, $term, $options['radius'], $options['ending']),
+                $term,
+                $options['highlight']
+            );
+        }
+        return trim($snippets);
     }
+
 }
