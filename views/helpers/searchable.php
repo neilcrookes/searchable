8c8f478e08226ca7b8cb92a71ac7d6e42462b888
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-01-19 22:08:12 -0500

    100% unit test coverage for SearchableHelper

--- views/helpers/searchable.php
+++ views/helpers/searchable.php
@@ -1,7 +1,43 @@
 <?php
+/**
+ * Searchable helper library.
+ *
+ * Allows manipulation of search data
+ *
+ * @package       searchable
+ * @subpackage    searchable.view.helpers
+ */
 class SearchableHelper extends AppHelper {
-    var $helpers = array('Html', 'Text');
 
+/**
+ * Included helpers.
+ *
+ * @var array
+ */
+    var $helpers = array('Text');
+
+/**
+ * Contains fallback record data
+ *
+ * @var string
+ */
+    var $recordData = null;
+
+/**
+ * Highlights the term within the search_index data
+ *
+ * ### Options:
+ * 
+ * - `field` Field used in view->data[SearchIndex] which contains the search term
+ * - `maxLength` Max length of highlighted text
+ * - `radius` The amount of characters that will be returned on each side of the founded phrase
+ * - `ending` Ending that will be appended
+ * - `highlight` Array of options for TextHelper::highlight()
+ *
+ * @param Object $data Object containing data
+ * @param array $options An array of options
+ * @return string highlighted search snippet
+ */
     function snippets($data, $options = array()) {
         $options = array_merge(array(
             'field' => 'term',
@@ -28,6 +64,42 @@ class SearchableHelper extends AppHelper {
             );
         }
         return trim($snippets);
+    }
+
+/**
+ * Returns unhashed data
+ *
+ * @param string $field Name of key in $data to be returned
+ * @param mixed $data Object containing value to be returned or null
+ * @return void
+ * @author Jose Diaz-Gonzalez
+ */
+    function data($field, $data = null) {
+        $field = sha1($field);
+
+        if (empty($data) && !empty($this->recordData)) {
+            $data = $this->recordData;
+        }
+
+        if (!is_object($data)) {
+            return false;
+        }
+
+        if (isset($data->{$field})) {
+            return $data->{$field};
+        }
+        return false;
+    }
+
+/**
+ * Sets a record for use with Searchable::data()
+ *
+ * @param Object $data Object containing data
+ * @return void
+ * @author Jose Diaz-Gonzalez
+ **/
+    function setRecord($data = null) {
+        $this->recordData = $data;
     }
 
 }
